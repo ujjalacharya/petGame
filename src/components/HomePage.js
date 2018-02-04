4833a876649a5e6539ddac26d9f92e235be2879b
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-02-04 21:50:03 +0545

    used ref for child component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -18,9 +18,25 @@ var btnStyle = {
 
 
 class HomePage extends React.Component {
+    constructor(props){
+        super(props);
+        this.catCompInstRef = null;
+        this.dogCompInstRef = null;
+        
+    }
 
     handleShowWinner = ()=>{
-        console.log("SHow winner clicked")
+        
+        let catLikes = this.catCompInstRef.state.likesCount;
+        let dogLikes = this.dogCompInstRef.state.likesCount;
+        if(catLikes > dogLikes){
+            console.log("Cat is the winner")
+        }
+        else if(catLikes < dogLikes){
+            console.log("Dog is the winner")
+        }else{
+            console.log("It's a tie")
+        }
     }
 
     
@@ -38,10 +54,12 @@ class HomePage extends React.Component {
                     <PetComponent
                         petName="Cat"
                         petImageUrl="https://i.ytimg.com/vi/W-PBFMECvTE/maxresdefault.jpg"
+                        ref = {(compInst)=>{this.catCompInstRef = compInst }}
                     />
                     <PetComponent
                         petName="Dog"
                         petImageUrl="http://www.petguide.com/wp-content/uploads/2013/05/cute-dog-names-12.jpg"
+                        ref = {(compInst)=>{this.dogCompInstRef = compInst}}
                     />
 
                 </div>
